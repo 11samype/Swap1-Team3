442eaa0fa7b067470dcfd8844b76ae4cda38754f
Teddy Sterne <sternetj@rose-hulman.edu>
2014-03-30 21:07:18 -0400

    day conversion class

---
+++ Swap 2/swap1/swap1/src/scheduleGenerator/DayConversion.java
@@ -0,0 +1,24 @@
+package scheduleGenerator;
+
+public class DayConversion {
+
+	public static int numForName(String nameOfDay) {
+		int dayNum = 0;
+		if (nameOfDay.equals("Sunday")) {
+			dayNum = 1;
+		} else if (nameOfDay.equals("Monday")) {
+			dayNum = 2;
+		} else if (nameOfDay.equals("Tuesday")) {
+			dayNum = 3;
+		} else if (nameOfDay.equals("Wednesday")) {
+			dayNum = 4;
+		} else if (nameOfDay.equals("Thursday")) {
+			dayNum = 5;
+		} else if (nameOfDay.equals("Friday")) {
+			dayNum = 6;
+		} else if (nameOfDay.equals("Saturday")) {
+			dayNum = 7;
+		}
+		return dayNum;
+	}
+}
